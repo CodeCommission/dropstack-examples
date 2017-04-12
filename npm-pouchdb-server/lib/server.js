5ac847f893d68e13f95a1ef06e4d1bdcf97b528a
Mike Bild <mike.bild@googlemail.com>
2017-04-12 19:28:40 +0200

    Remove unused deps

--- npm-pouchdb-server/lib/server.js
+++ npm-pouchdb-server/lib/server.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const expressCors = require('cors');
 const expressMorgan = require('morgan');
-const expressBodyParser = require('body-parser');
 const PouchDB = require('pouchdb');
 PouchDB.plugin(require('pouchdb-adapter-node-websql'))
 const DefaultPouch = PouchDB.defaults({prefix: 'databases/', adapter: 'websql'});
@@ -10,18 +9,18 @@ const expressPouchDB = require('express-pouchdb')(DefaultPouch, {
   configPath: 'pouchdb-config.json',
   mode: 'fullCouchDB',
 });
-const databases = {};
-const app = express();
 
+const app = express();
 app.disable('x-powered-by');
 app.use(expressMorgan('dev'));
 app.use(expressCors());
 app.use(expressPouchDB);
-app.use(expressBodyParser.json({limit: '1mb'}));
 
 let server = undefined;
 module.exports = {
   start: ({port, isMemory = false }) => {
+    const databases = {};
+
     return new Promise(resolve => {
       databases.test = new DefaultPouch('test');
       server = app.listen(port, () => resolve({
