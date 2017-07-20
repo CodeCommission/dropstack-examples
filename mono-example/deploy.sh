f46bb8caf456c4db89b61769441fd3384e9f3a6b
Ralf Westphal <ralfw@users.noreply.github.com>
2017-07-20 22:13:50 +0200

    updated deployment script (#8)
    * cleared old demo solution
    
    * Very simple Mono example with test
    
    * Added functionality with tests
    
    3 endpoints for hello-greetings, one with a small database
    
    * build /bin under root of sample
    
    * adjusted scripts and readme
    
    * added provocative endpoints
    
    * enhanced readme
    
    * Addapted to new version of dropstack: scripts to run on server are passed on command line, see deploy.sh
    
    * Added test script
    
    * enhance docu, renamed start scripts to „run“
    
    * fixed wrong script name
    
    * Updated deployment script

--- mono-example/deploy.sh
+++ mono-example/deploy.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-dropstack deploy -v build=./ds-build.sh -v test=./ds-test.sh -v start=./ds-run.sh
+dropstack deploy -v build=./ds-build.sh -v test=./ds-test.sh -v run=./ds-run.sh --verbose
