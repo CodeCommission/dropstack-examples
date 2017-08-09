54de4e8ff701dcc9904a5df23f73cdd873a9b308
Mike Bild <mike.bild@googlemail.com>
2017-08-09 10:14:33 +0200

    Haskell dockerized

--- haskell-example/Main.hs
+++ haskell-example/Main.hs
@@ -6,4 +6,4 @@ main :: IO ()
 main = scotty 80 $ do
   get "/:word" $ do
     beam <- param "word"
-    html $ mconcat ["<h1>Scotty, ", beam, " me up!</h1>"]
+    html $ mconcat ["<h1>Dropstack, ", beam, " me up!</h1>"]
