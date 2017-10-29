501b9cdb9553519ad46f653f418b8dbabfcb7705
Mike Bild <mike.bild@googlemail.com>
2017-10-29 20:10:33 +0100

    Add Env-Var SERVICE_PORT

--- npm-example/server.js
+++ npm-example/server.js
@@ -1,6 +1,11 @@
-const express = require('express');
+const express = require("express");
 const app = express();
+const SERVICE_PORT = process.env.SERVICE_PORT || 3000;
 
-app.get('/', (req, res, next) => res.send({message: '(GET/TEXT) DROPSTACK NodeJS Example'}));
+app.get("/", (req, res, next) =>
+  res.send({ message: "(GET/TEXT) DROPSTACK NodeJS Example" })
+);
 
-const server = app.listen(80, () => console.log(`Listen on ${server.address().port}`));
+const server = app.listen(SERVICE_PORT, () =>
+  console.log(`Listen on ${server.address().port}`)
+);
