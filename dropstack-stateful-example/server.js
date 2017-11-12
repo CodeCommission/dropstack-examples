e8b51a87aa86e17bee7942dbaedf34db056ef14e
Mike Bild <mike.bild@googlemail.com>
2017-11-12 05:59:59 +0100

    Stateful service example

---
+++ dropstack-stateful-example/server.js
@@ -0,0 +1,20 @@
+const SERVICE_PORT = process.env.SERVICE_PORT || 8080;
+const fse = require("fs-extra");
+const path = require("path");
+const express = require("express");
+const app = express();
+
+app.get("/files", (req, res) => {
+  fse.readdir(path.resolve("/mnt")).then(data => res.send(data));
+});
+
+app.listen(SERVICE_PORT, () => console.log(`Listen on ${SERVICE_PORT}`));
+
+let count = 0;
+setInterval(() => {
+  count++;
+  fse.writeFile(
+    path.resolve(`/mnt/file-${count}-${new Date().toISOString()}.json`),
+    JSON.stringify({ id: count })
+  );
+}, 5000);
